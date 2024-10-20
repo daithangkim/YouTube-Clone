e62b505277a0c573a78b0909f1c323a10e77f280
Dai-Thang <daithangkim1@gmail.com>
2024-10-21 00:10:18 +0200

    recommendation buttons on home

--- src/pages/Home/homeRecommendationButtons.jsx
+++ src/pages/Home/homeRecommendationButtons.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import {Button} from "@mui/material";
+import {Box, Button} from "@mui/material";
 
 const HomeRecommendationButtons = () => {
     return (
-        <div>
+        <Box>
             {['All', 'Music', 'JavaScript', 'Boxing', 'Mixes', 'Sparring', 'Stocks', 'Podcasts', 'AI']
                 .map((recommendation, index) => (
-                    <Button key={index}
-                            sx={{
-                                backgroundColor: 'grey.400', borderRadius: 20, margin: 1, color: 'black'
-                            }}>{recommendation}</Button>
+                    <Button
+                        key={index}
+                        sx={{
+                            backgroundColor: 'grey.200',
+                            borderRadius: 5,
+                            padding: 1,
+                            margin: 1,
+                            color: 'black',
+                            '&:hover': {
+                                backgroundColor: 'grey.300',
+                            },
+                        }}
+                    >
+                        {recommendation}
+                    </Button>
                 ))}
-        </div>
+        </Box>
     );
 }
 
