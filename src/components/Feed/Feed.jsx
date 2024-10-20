bacf5f966a6c74d36954fe4141cd0e9f90f85b5a
Dai-Thang <daithangkim1@gmail.com>
2024-10-21 00:42:34 +0200

    removed the box borders and boxShadow

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { Grid, Typography } from '@mui/material';
+import React, {useEffect, useState} from 'react';
+import {Box, Button, Grid, Typography} from '@mui/material';
 import axios from 'axios';
-import { useDispatch, useSelector } from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import VideoCard from "../VideoCard/VideoCard";
 import HomeRecommendationButtons from "../../pages/Home/homeRecommendationButtons";
+import TheatersIcon from "@mui/icons-material/Theaters";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
 
 const Feed = () => {
     const dispatch = useDispatch();
     const selectedCategory = useSelector((state) => state.category.selectedCategory);
-    const [videos, setVideos] = useState([]); // State to store the fetched data
+    const [videos, setVideos] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -23,35 +25,71 @@ const Feed = () => {
 
             try {
                 const response = await axios.request(options);
-                setVideos(response.data.data); // Update state with the fetched data
+                setVideos(response.data.data);
             } catch (error) {
                 console.error(error);
             }
         };
 
-        fetchData(); // Call the async function
+        fetchData();
     }, [selectedCategory]);
 
+    // Handle category click
+    const handleCategoryClick = (text) => {
+        dispatch({type: 'SET_SELECTED_CATEGORY', payload: text.toLowerCase()});
+    };
+
     return (
-        <div>
+        <Box>
             <HomeRecommendationButtons/>
-            <Grid container spacing={3} padding={2}>
-                {videos.length > 0 ? (
-                    // only extracting the videos and not the shorts
-                    videos
-                        .filter(video => video.type === 'video')
-                        .map((video, index) => (
-                            <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
-                                <VideoCard video={video} />
+            <Grid container padding={2}>
+                {/* Buttons on the left side */}
+                <Grid item xs={0.5}>
+                    <Box display="flex" flexDirection="column" paddingY={2}>
+                        {['Shorts', 'Subscriptions', 'You'].map((text) => (
+                            <Button
+                                key={text}
+                                onClick={() => handleCategoryClick(text)}
+                                sx={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                    borderRadius: '60px',
+                                    margin: 1,
+                                    backgroundColor: selectedCategory === text.toLowerCase() ? 'lightgray' : 'transparent',
+                                }}
+                            >
+                                {text === 'Shorts' ? <TheatersIcon/> : <SubscriptionsIcon/>}
+                                <Typography variant="caption" sx={{
+                                    fontSize: {xs: '0.4rem', sm: '0.45rem', md: '0.5rem'},
+                                    color: 'black',
+                                    textAlign: 'center'
+                                }}>{text}</Typography>
+                            </Button>
+                        ))}
+                    </Box>
+                </Grid>
+
+                {/* Videos on the right side */}
+                <Grid item xs={11.5}>
+                    <Grid container spacing={3}>
+                        {videos.length > 0 ? (
+                            videos
+                                .filter(video => video.type === 'video')
+                                .map((video, index) => (
+                                    <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+                                        <VideoCard video={video}/>
+                                    </Grid>
+                                ))
+                        ) : (
+                            <Grid item xs={12}>
+                                <Typography variant="h6">No videos available</Typography>
                             </Grid>
-                        ))
-                ) : (
-                    <Grid item xs={12}>
-                        <Typography variant="h6">No videos available</Typography>
+                        )}
                     </Grid>
-                )}
+                </Grid>
             </Grid>
-        </div>
+        </Box>
     );
 }
 
--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -3,34 +3,41 @@ import { Card, CardContent, CardMedia, Typography } from '@mui/material';
 import { styled } from '@mui/system';
 
 // Styled CardMedia with hover effect and cursor change
-const StyledCardMedia = styled(CardMedia)({
+const StyledCardMedia = styled(CardMedia)(() => ({
     transition: 'transform 0.3s ease',
     cursor: 'pointer',
     '&:hover': {
         transform: 'scale(1.05)',
     },
-});
+}));
 
 const VideoCard = ({ video }) => {
     const {
         title,
         channelTitle,
-        description,
         thumbnail,
         viewCount,
         publishedTimeText,
     } = video;
 
     return (
-        <Card style={{ display: 'flex', flexDirection: 'column', height: '100%', border: 'none' }}>
+        <Card
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100%',
+                boxShadow: 'none',
+                border: 'none',
+            }}
+        >
             <StyledCardMedia
                 component="img"
                 height="177"
                 image={thumbnail[0]?.url}
                 alt={title}
             />
-            <CardContent style={{ padding: '16px', backgroundColor: 'transparent' }}>
-                <Typography variant="h7" gutterBottom style={{ fontWeight: 'bold' }}>
+            <CardContent sx={{ padding: '16px', backgroundColor: 'transparent' }}>
+                <Typography variant="h7" gutterBottom sx={{ fontWeight: 'bold' }}>
                     {title}
                 </Typography>
                 <Typography variant="subtitle1" color="textSecondary" gutterBottom>
